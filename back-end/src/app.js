498c131285578b3ae3b3a734ebfe2ce9d0bd179e
Thales <thales.sousufpi@gmail.com>
2022-04-02 18:36:20 -0300

    Criando banco de dados Mongo na nuvem

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 const router = require('./routes/routes.js')
+const mongoose = require('./database/database')
 app.use(morgan('dev'))
 app.use(router)
 module.exports = app
---
+++ back-end/src/database/database.js
@@ -0,0 +1,2 @@
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://root:root123@cluster0.dgju2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority').then(console.log('conectado'))
