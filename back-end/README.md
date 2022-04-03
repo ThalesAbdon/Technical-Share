d1d368f44f35fc7563c7fedc02b43fea1230a302
Thales <thales.sousufpi@gmail.com>
2022-04-02 22:10:23 -0300

    Documentando a API

--- back-end/README.md
+++ back-end/README.md
@@ -25,3 +25,40 @@
 
 ### Mongo Atlas
 <p> Mongo Atlas é um cluster(em nuvem) que oferece armazenamento para dados utilizando MongoDB como Banco de Dados</p>
+
+
+## Como testar a API?
+
+<p>Para fazer testes utilize o seguinte url:</p>
+
+* Rota de cadastro http://localhost:4000/api/create
+```
+{
+"name": "Maria",
+"work": "Ux",
+"seniority": "Senior",
+"skills": "MVP",
+"bio": "Testando segundo usuário"
+}
+```
+
+* Rota de listar http://localhost:4000/api/get
+```
+#No body
+```
+* Rota de atualizar http://localhost:4000/api/update/:id
+```
+{
+#No lugar de :id na url, colocar o id do usuário( que está sendo gerado pelo mongo)   
+"name": "Maria Lurdes",
+}
+```
+* Rota de atualizar images http://localhost:4000/api/update/:id
+```   
+#Utilize o multform do insomnia com 2 campos: 
+#1° campo de nome: avatar   2° campo com o arquivo que deseja upar: exemplo.png
+```
+* Rota de deletar http://localhost:4000/api/update/:id
+```
+#No lugar de :id na url, colocar o id do usuário( que está sendo gerado pelo mongo)   
+```
--- back-end/src/controller/User.js
+++ back-end/src/controller/User.js
@@ -1,40 +1,43 @@
-const UserModel = require('../models/User')
+const UserModel = require("../models/User");
 
-class User{
-    async get(req,res){
-        const user = await UserModel.find()
-        return res.status(200).json({user})
-    }
+class User {
+  async create(req, res) {
+    const { name, work, seniority, skills, bio } = req.body;
+    await UserModel.create({ name, work, seniority, skills, bio });
+    return res.status(201).json({ message: "Usuário criado!" });
+  }
 
-    async create(req,res){
-       const {name,work,seniority,skills,bio} = req.body 
-       await UserModel.create({name,work,seniority,skills,bio})
-       return res.status(201).json({message:"Usuário criado!"})
-    }
+  async get(req, res) {
+    const user = await UserModel.find();
+    return res.status(200).json({ user });
+  }
 
-    async update(req,res){
-       
-       
-        if(req.file){
-            const avatar = `http://localhost:4000/files/images/${req.file.filename}`
-            console.log(avatar)
-            await UserModel.findByIdAndUpdate({_id:req.params.id},{
-                $set: {avatar}
-                
-            })
-            return res.status(200).json({message:"Usuário atualizado!"})
-        } else{
-            await UserModel.findByIdAndUpdate({_id:req.params.id},{
-                $set: req.body
-            })
-            return res.status(200).json({message:"Usuário atualizado!"})
+  async update(req, res) {
+    if (req.file) {
+      const avatar = `http://localhost:4000/files/images/${req.file.filename}`;
+      console.log(avatar);
+      await UserModel.findByIdAndUpdate(
+        { _id: req.params.id },
+        {
+          $set: { avatar },
         }
+      );
+      return res.status(200).json({ message: "Usuário atualizado!" });
+    } else {
+      await UserModel.findByIdAndUpdate(
+        { _id: req.params.id },
+        {
+          $set: req.body,
+        }
+      );
+      return res.status(200).json({ message: "Usuário atualizado!" });
     }
+  }
 
-    async delete(req,res){
-            await UserModel.findByIdAndDelete({_id:req.params.id})
-            return res.status(200).json({message:"Usuário Deletado!"})
-    }
+  async delete(req, res) {
+    await UserModel.findByIdAndDelete({ _id: req.params.id });
+    return res.status(200).json({ message: "Usuário Deletado!" });
+  }
 }
 
-module.exports = new User()
+module.exports = new User();
