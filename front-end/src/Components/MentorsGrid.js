17d12ddd8b47ecef6d0c6d5a287b0080c04e2583
Analu <contato@analugmrs.com>
2022-04-11 17:57:03 -0300

    test: search bar

--- front-end/src/Components/MentorsGrid.js
+++ front-end/src/Components/MentorsGrid.js
@@ -11,6 +11,7 @@ import Calendar from 'react-calendar'
 import 'react-calendar/dist/Calendar.css';
 import moment from 'moment';
 import Auth from "../Auth/Auth";
+import buscar from './Search'
 
 export default function MentorsGrid() {
 
--- front-end/src/Components/Search.js
+++ front-end/src/Components/Search.js
@@ -1,21 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './Search.scss';
 import {Container, InputGroup, FormControl, Row} from 'react-bootstrap';
+import api from '../services/api';
 
-document.body.style = "background: transparent";
 
-const Search = () => (
+export default function Search () {
+    
+    document.body.style = "background: transparent";
+
+    const [search,setSearch] = useState('')
+
+   // useEffect(() => {
+     async function buscar(){
+     const data = await api.get(`/api/search/?skills=${search}`)
+     console.log(data.data, true)
+
+     //setSearch(data.data)
+     }
+    //})
+
+     return(
+
     <section className="d-flex px-3">
         <Container>
             <Row className="justify-content-center">
                 <h1>Encontre um <b>mentor</b></h1>
                 <InputGroup className="input py-3" size="sm">
-                    <FormControl aria-label="Large" aria-describedby="inputGroup-sizing-md" placeholder="Digite aqui uma skill que você quer aprender"/>                    
+                    <FormControl aria-label="Large" aria-describedby="inputGroup-sizing-md" placeholder="Digite aqui uma skill que você quer aprender" value={search}onChange = {e=>setSearch(e.target.value)}/><button onClick={buscar}>search</button>                
                 </InputGroup>
                 <h2>Estes são os mentores com a <b>skill escolhida</b></h2>
             </Row>
         </Container>
     </section>
-)
-
-export default Search;
+     )
+}
