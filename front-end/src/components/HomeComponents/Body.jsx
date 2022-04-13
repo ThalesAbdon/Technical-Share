3ac1750e834842224ba2ec4eb3ff9a0e7a1b3a8c
Vitoria Maia <97573142+vitoriadev@users.noreply.github.com>
2022-04-12 23:26:14 -0300

    vi

--- front-end/src/components/HomeComponents/Body.jsx
+++ front-end/src/components/HomeComponents/Body.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom"
+import foguete from "../../assets/foguete.png"
+
 import "./Body.css"
 function Body() {
 
@@ -11,21 +13,30 @@ function Body() {
 
     return (
         <React.Fragment>
-            <section className="home-container">
-                <div className="home-titulos-container">
-                    <h1>Olá,</h1><h1>#sanguelaranja</h1>
+            <section className="home-big-container">
+                <div className="home-imagem-foguete-container">
+                    <img src={foguete} alt="Foguete" />
                 </div>
-                <div className="home-sub-titulos-container">
-                    <h3>Bem-vindo à sua plataforma exclusiva de mentorias.</h3>
+                <div className="home-container">
+                    <div className="home-titulos-container">
+                        <h1>Olá,&nbsp;</h1>
+                        <h1>#sanguelaranja</h1>
+                    </div>
+                    <div className="home-sub-titulos-container">
+                        <h3>Bem-vindo à sua plataforma exclusiva de mentorias.</h3>
+                    </div>
+                    <div className="home-mensagens-container">
+                        <p>Aqui você pode ensinar, aprender, ajudar a tirar dúvidas, trocar experiências,</p>
+                        <p>conhecer seus colegas #sanguelaranja,</p>
+                        <p>pedir orientação e fazer agendamentos de mentorias.</p>
+                        <p>Tudo simples, rápido e on-line.</p>
+                    </div>
+                    <div className="home-button-comecar-container">
+                        <button className="botao" onClick={() => { redirecionarCadastro() }}>COMEÇAR</button>
+                    </div>
                 </div>
-                <div className="home-mensagens-container">
-                    <p>Aqui você pode ensinar, aprender, ajudar a tirar dúvidas, trocar experiências,</p>
-                    <p>conhecer seus colegas #sanguelaranja,</p>
-                    <p>pedir orientação e fazer agendamentos de mentorias.</p>
-                    <p>Tudo simples, rápido e on-line.</p>
-                </div>
-                <button className="botao" onClick={() => { redirecionarCadastro() }}>COMEÇAR</button>
             </section>
+
         </React.Fragment>
     )
 }
