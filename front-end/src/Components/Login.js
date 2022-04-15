756e3088c4cb70fdc16393765294eefe1c96ee34
Analu <contato@analugmrs.com>
2022-04-14 21:22:27 -0300

    feat: change type size in login and added login button in buttons file

--- front-end/src/Components/Login.js
+++ front-end/src/Components/Login.js
@@ -5,6 +5,7 @@ import api from '../services/api'
 import { useNavigate } from 'react-router-dom';
 import swal from '@sweetalert/with-react';
 import './Alert.scss';
+import './Buttons.scss';
 
 document.body.style = "background: transparent";
 
@@ -14,50 +15,46 @@ function Login() {
     const [senha,setSenha] = useState('');
     const history = useNavigate('');
 
-        async function login(e){
-            e.preventDefault(); 
-            try{
-                const data = await api.post("/api/login/", {email,senha})
-                console.log(data.status)
-                localStorage.setItem("token",data.data.token);
-                return history("/buscar")    
-            }catch(error){
-                swal({
-                    title: "E-mail ou senha incorretos",
-                    icon: "error",
-                  });
-            }
-        
+    async function login(e){
+        e.preventDefault(); 
+        try {
+            const data = await api.post("/api/login/", {email,senha})
+            console.log(data.status)
+            localStorage.setItem("token",data.data.token);
+            return history("/buscar")    
+        } catch(error){
+            swal({
+                title: "E-mail ou senha incorretos",
+                icon: "error",
+          });
         }
+    }
    
     return(
     <section className="d-flex">
         <Container className="align-items-center">
-          <Row className="justify-content-center center mt-5 ">
+          <Row className="justify-content-center mt-5 ">
                 <h1>Olá, <span>#sanguelaranja</span></h1>
-                <h5 className="mt-4">Cadastre-se para entrar nessa vitamina de conhecimento e ajuda mútua</h5>
-                <center className="box mt-5">
-                    <Form onSubmit= {login} >
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>E-mail corporativo</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" value={email} onChange = {e => setEmail(e.target.value)}/>
-                    </Form.Group>
-                         
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Label>Senha</Form.Label>
-                        <Form.Control type="password" placeholder="Password" value={senha} onChange = {e => setSenha(e.target.value)}/>
-                    </Form.Group>
-                    <br/>
-                    <Button variant="" className="color-primary" type="submit">
-                        Entrar
-                    </Button>                             
+                <h5 className="mt-4">Faça login para entrar nessa vitamina de conhecimento e ajuda mútua</h5>
+                <center className="my-5">
+                    <Form onSubmit= {login} className="justify-content-center">
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>E-mail corporativo</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" value={email} onChange = {e => setEmail(e.target.value)}/>
+                        </Form.Group>
+                            
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Senha</Form.Label>
+                            <Form.Control type="password" placeholder="Password" value={senha} onChange = {e => setSenha(e.target.value)}/>
+                        </Form.Group>
+                        <br/>
+                        <button variant="" className="login" type="submit"> Entrar </button>                             
                     </Form>
                 </center>
             </Row>
         </Container>
     </section>
     );
-
 }
 
 export default Login;
