83cdd5edf29a511e56babf98bc150eb9ac05f08d
Analu <contato@analugmrs.com>
2022-04-15 14:53:45 -0300

    ajustes no cod da vivi pra integrar a home

--- front-end/src/Components/Body.js
+++ front-end/src/Components/Body.js
@@ -2,8 +2,9 @@ import React from "react";
 import { useNavigate } from "react-router-dom"
 import { Container, Row, Col } from 'react-bootstrap';
 import foguete from "../resources/foguete.png"
+import './Buttons.scss';
 
-import "./Body.css"
+import "./Body.scss"
 function Body() {
 
     const navigate = useNavigate();
@@ -14,30 +15,32 @@ function Body() {
 
     return (
         <React.Fragment>
-            <section>
-            <Container className="my-5 py-5">
-                <Row>
-                    <Col>
+            <section className="d-flex body">
+            <Container className="my-5 py-5 text-right">
+                <Row className="justify-content-end">
+                    <Col className="justify-content-end">
+                    {/* {/* <Col md={4}> */}
                 <div className="home-imagem-foguete-container">
                     <img src={foguete} alt="Foguete" />
                 </div>
-                </Col>
-                <Col>
+                {/* </Col>
+                <Col md={8}> */} 
                     {/* <div className="home-titulos-container"> */}
-                        <h1>Olá, <b>#sanguelaranja</b></h1>
+                        <h1 className="mb-4">Olá, <b>#sanguelaranja</b></h1>
                     {/* </div>
                     <div className="home-sub-titulos-container"> */}
-                        <h3>Bem-vindo à sua plataforma exclusiva de mentorias.</h3>
+                        <h2 className="mb-5">Bem-vindo à sua plataforma exclusiva de mentorias.</h2>
                     {/* </div>
                     <div className="home-mensagens-container"> */}
-                        <p>Aqui você pode ensinar, aprender, ajudar a tirar dúvidas, trocar experiências,</p>
-                        <p>conhecer seus colegas #sanguelaranja,</p>
-                        <p>pedir orientação e fazer agendamentos de mentorias.</p>
-                        <p>Tudo simples, rápido e on-line.</p>
+                        <p>Aqui você pode ensinar, aprender, ajudar a tirar dúvidas, trocar experiências,<br/>
+                        conhecer seus colegas #sanguelaranja,<br/>
+                        pedir orientação e fazer agendamentos de mentorias.<br/>
+                        Tudo simples, rápido e on-line.</p>
                     {/* </div> */}
-                    <div className="home-button-comecar-container">
-                        <button className="botao" onClick={() => { redirecionarCadastro() }}>COMEÇAR</button>
+                    <div className="home-button-comecar-container mt-5">
+                        <button className="start mt-5" onClick={() => { redirecionarCadastro() }}>Começar</button>
                     </div>
+                {/* </Col> */}
                 </Col>
                 </Row>
             </Container>
