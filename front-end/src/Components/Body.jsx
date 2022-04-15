20f5195e0fbd8fd1814d76ebf4fedb2c305365d5
Analu <contato@analugmrs.com>
2022-04-15 12:40:54 -0300

    reestruturando o cod da vivi

--- front-end/src/Components/Body.jsx
+++ front-end/src/Components/Body.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom"
+import { Container, Row, Col } from 'react-bootstrap';
 import foguete from "../resources/foguete.png"
 
 import "./Body.css"
@@ -13,30 +14,34 @@ function Body() {
 
     return (
         <React.Fragment>
-            <section className="home-big-container">
+            <section>
+            <Container className="my-5 py-5">
+                <Row>
+                    <Col>
                 <div className="home-imagem-foguete-container">
                     <img src={foguete} alt="Foguete" />
                 </div>
-                <div className="home-container">
-                    <div className="home-titulos-container">
-                        <h1>Olá,&nbsp;</h1>
-                        <h1>#sanguelaranja</h1>
-                    </div>
-                    <div className="home-sub-titulos-container">
+                </Col>
+                <Col>
+                    {/* <div className="home-titulos-container"> */}
+                        <h1>Olá, <b>#sanguelaranja</b></h1>
+                    {/* </div>
+                    <div className="home-sub-titulos-container"> */}
                         <h3>Bem-vindo à sua plataforma exclusiva de mentorias.</h3>
-                    </div>
-                    <div className="home-mensagens-container">
+                    {/* </div>
+                    <div className="home-mensagens-container"> */}
                         <p>Aqui você pode ensinar, aprender, ajudar a tirar dúvidas, trocar experiências,</p>
                         <p>conhecer seus colegas #sanguelaranja,</p>
                         <p>pedir orientação e fazer agendamentos de mentorias.</p>
                         <p>Tudo simples, rápido e on-line.</p>
-                    </div>
+                    {/* </div> */}
                     <div className="home-button-comecar-container">
                         <button className="botao" onClick={() => { redirecionarCadastro() }}>COMEÇAR</button>
                     </div>
-                </div>
+                </Col>
+                </Row>
+            </Container>
             </section>
-
         </React.Fragment>
     )
 }
